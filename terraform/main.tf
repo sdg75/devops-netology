91c85ad906fb3f284730237f02f208e28088e327
Dmitry Semenov <dmtsm@mail.ru>
2022-09-06 22:48:42 +0300

    7_2 - Homework 1.0

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+resource "yandex_compute_instance" "web" {
+  name                      = "web"
+  zone                      = "ru-central1-a"
+  hostname                  = "web.netology.yc"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-web"
+      type        = "network-nvme"
+      size        = "10"
+    }
+  }
+
+  network_interface {
+    subnet_id  = "${yandex_vpc_subnet.default.id}"
+    nat        = true
+    ip_address = "192.168.101.11"
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1g1dqi6610hmdn5ar0l"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g0t2q3lpao0dssjgj9"
+}
+
+variable "centos-7-base" {
+  default = "fd80sq3nsnjk174e42hu"
+}
